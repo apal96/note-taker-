f8afe6a997e9d85d37b8d032538f73dfe57f0bb7
apal <alishapal96@gmail.com>
2021-08-19 23:01:42 -0700

    add route to post new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const path = require ('path');
 const router = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 //read the db.json file and return saved notes as JSON
 router.get('/notes',(req,res)=>{
@@ -11,6 +12,26 @@ router.get('/notes',(req,res)=>{
 
 //recieve new note to save on the request body, add to db.json, then return new note to the client
 router.post('/notes',(req,res)=>{
+    const {title, text} = req.body;
+
+    if(title &&text){
+        const newNote = {
+            title,
+            text,
+            note_id:uuidv4(),
+        };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting Note');
+  }
 
 })
 
