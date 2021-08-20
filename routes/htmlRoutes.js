cedf72ae5797a0670aca3f7d19791ab1b495a6eb
apal <alishapal96@gmail.com>
2021-08-19 22:20:55 -0700

    add route to get index html page

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+const path = require ('path');
+const html = require('express').Router();
+//return the notes.html file
+html.get('/notes',(req,res)=>{
+res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+//return the index.html file
+html.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html')));
+
+module.exports = html;
