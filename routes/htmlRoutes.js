48e348a3e63ecc173f556133346696c70ee317f0
apal <alishapal96@gmail.com>
2021-08-19 22:21:59 -0700

    add route to get notes html page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require ('path');
 const html = require('express').Router();
 //return the notes.html file
 html.get('/notes',(req,res)=>{
-res.sendFile(path.join(__dirname,'../public/index.html'))
+res.sendFile(path.join(__dirname,'../public/notes.html'))
 })
 //return the index.html file
 html.get('*', (req, res) =>
